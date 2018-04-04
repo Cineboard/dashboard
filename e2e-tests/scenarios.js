7282fd82953db0f8e35c5d872e2777d8ef435c4e
gionni <giovbat@gmail.com>
2018-04-04 02:38:06 +0200

    protractor: browser.getLocationAbsUrl() deprecated. fixed.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,7 +7,7 @@ describe('my app', function() {
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getCurrentUrl()).toMatch("/view1");
   });
 
 
