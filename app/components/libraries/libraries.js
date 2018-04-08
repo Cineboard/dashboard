16e1dff17273394036807d4722bf558562d20844
gionni <giovbat@gmail.com>
2018-04-08 08:17:47 +0200

    base libraries view/controller

--- app/components/libraries/libraries.js
+++ app/components/libraries/libraries.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('Cineboard.libraries', ['ngRoute'])
+angular.module('Cineboard.libraries', ['ngRoute', 'data-table'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/libraries', {
@@ -10,6 +10,39 @@ angular.module('Cineboard.libraries', ['ngRoute'])
     }])
 
     .controller('LibrariesController', ['$scope', 'LibrariesService', function ($scope, LibrariesService) {
+        $scope.options = {
+            rowHeight: 100,
+            footerHeight: false,
+            scrollbarV: false,
+            headerHeight: 50,
+            //columnMode: 'flex',
+            selectable: true,
+            multiSelect: true,
+            columns: [{
+                name: "UserId",
+                prop: "user_id",
+                width: 300
+            }, {
+                name: "Title",
+                prop: "title"
+            }, {
+                name: "Rating",
+                prop: "rating"
+            }, {
+                name: "Tags",
+                prop: "tags"
+            }, {
+                name: "Notes",
+                prop: "notes"
+            }]
+        };
+        $scope.selected = [];
+        $scope.onSelect = function (row) {
+            console.log('ROW SELECTED!', row);
+        };
+        $scope.onRowClick = function (row) {
+            console.log('ROW CLICKED', row);
+        };
         LibrariesService.getLibraries().then(function (librariesData) {
             $scope.librariesData = librariesData;
             console.log(librariesData);
