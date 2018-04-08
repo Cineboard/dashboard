61317da3e93e889aea4a389445c17c8ee18ea5e5
gionni <giovbat@gmail.com>
2018-04-08 08:16:25 +0200

    e2e tests activated on CI: test

--- app/components/version/version.js
+++ app/components/version/version.js
@@ -5,4 +5,4 @@ angular.module('Cineboard.version', [
   'Cineboard.version.version-directive'
 ])
 
-.value('version', '0.1');
+.value('version', '0.3');
--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/',
+  baseUrl: 'http://localhost:8080/',
 
   framework: 'jasmine',
 
