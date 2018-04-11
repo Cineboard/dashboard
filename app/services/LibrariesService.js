9888878dcb1e94385104b6275d0bd7fb842af4d5
gionni <giovbat@gmail.com>
2018-04-11 20:05:15 +0200

    libraries service: save

--- app/services/LibrariesService.js
+++ app/services/LibrariesService.js
@@ -11,6 +11,21 @@ app.factory('LibrariesService', function ($rootScope, $http, CONFIG, $location) 
                 .then(function (result) {
                     return result.data.libraries;
                 });
+        },
+
+        saveLibrary: function (library) {
+            return $http.post(CONFIG.API_URL + '/libraries', {
+                'user_id': $rootScope.currentUser.id,
+                'title': library.title,
+                'director': library.director,
+                'viewed': library.viewed,
+                'rating': library.rating,
+                'tags': library.tags,
+                'url': library.url
+            })
+                .then(function (result) {
+                    return result.data;
+                });
         }
     };
 });
